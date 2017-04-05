2a7a6c3287e6a59d9aa13ef7e72f24c6e221813d
rsk5492 <ssk5492@gmail.com>
2017-04-05 03:35:18 -0700

    Initial commit

---
+++ src/main/java/com/gamerzone/GamerZoneApplication.java
@@ -0,0 +1,12 @@
+package com.gamerzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerZoneApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gamerzone/GamerZoneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gamerzone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamerZoneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
